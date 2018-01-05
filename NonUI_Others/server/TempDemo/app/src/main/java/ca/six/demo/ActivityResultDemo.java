9322493899ba1c186d424a5e7c2630e608819fa9
songzhw <songzhw2012@gmail.com>
2018-01-05 18:24:38 -0500

    init the demo

---
+++ NonUI_Others/server/TempDemo/app/src/main/java/ca/six/demo/ActivityResultDemo.java
@@ -0,0 +1,32 @@
+package ca.six.demo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by songzhw on 2018/1/5.
+ */
+public class ActivityResultDemo extends AppCompatActivity implements View.OnClickListener {
+    private TextView tvMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvMain = (TextView) findViewById(R.id.tvMain);
+        tvMain.setText("Page B");
+        tvMain.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        setResult(RESULT_OK);
+        this.finish();
+    }
+
+}

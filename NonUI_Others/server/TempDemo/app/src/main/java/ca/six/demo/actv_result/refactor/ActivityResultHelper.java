8302dd65e814c38e35d8750dde9af68219248897
songzhw <songzhw2012@gmail.com>
2018-01-05 21:40:49 -0500

    onActivityResult trick

--- NonUI_Others/server/TempDemo/app/src/main/java/ca/six/demo/actv_result/refactor/ActivityResultHelper.java
+++ NonUI_Others/server/TempDemo/app/src/main/java/ca/six/demo/actv_result/refactor/ActivityResultHelper.java
@@ -1,8 +1,18 @@
 package ca.six.demo.actv_result.refactor;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
 /**
  * Created by songzhw on 2018/1/5.
  */
 
 public class ActivityResultHelper {
+    public void startActivityForResult(Context context, Class<Activity> target) {
+        Intent it = new Intent(context, ImplicitResultActivity.class);
+        it.putExtra("target_class", target);
+        context.startActivity(it);
+    }
+    // TODO Supporting IntentFlag, IntentParams ...
 }
--- NonUI_Others/server/TempDemo/app/src/main/java/ca/six/demo/actv_result/refactor/ImplicitResultActivity.java
+++ NonUI_Others/server/TempDemo/app/src/main/java/ca/six/demo/actv_result/refactor/ImplicitResultActivity.java
@@ -1,10 +1,35 @@
 package ca.six.demo.actv_result.refactor;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 /**
- * Created by songzhw on 2018/1/5.
+ * Implicit Activity. You should never use it.
+ * The only way to use this Activity is to use it through {@link ActivityResultHelper}.
  */
 
 public class ImplicitResultActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent it = getIntent();
+        Class<Activity> target = (Class<Activity>) it.getSerializableExtra("target_class");
+        this.startActivityForResult(it, 11);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode != 11) {
+            return;
+        }
+
+        if (resultCode == RESULT_OK) {
+
+        } else {
+
+        }
+    }
 }

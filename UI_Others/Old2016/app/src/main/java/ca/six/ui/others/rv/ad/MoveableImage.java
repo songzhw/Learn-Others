cf6f488d199cd07a47c558a6bfb9686e766dda08
songzhw <songzhw2012@gmail.com>
2018-05-04 16:52:38 -0400

    remove ViewDragHelper, add onTouchEvent to insert our logic

--- UI_Others/Old2016/app/src/main/java/ca/six/ui/others/rv/ad/MoveableImage.java
+++ UI_Others/Old2016/app/src/main/java/ca/six/ui/others/rv/ad/MoveableImage.java
@@ -1,14 +1,16 @@
 package ca.six.ui.others.rv.ad;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.widget.ViewDragHelper;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
+
+import static android.view.MotionEvent.ACTION_DOWN;
+import static android.view.MotionEvent.ACTION_MOVE;
+import static android.view.MotionEvent.ACTION_UP;
 
 public class MoveableImage extends AppCompatImageView {
     public MoveableImage(Context context) {
@@ -21,28 +23,46 @@ public class MoveableImage extends AppCompatImageView {
         init(context, attrs);
     }
 
-    private ViewDragHelper.Callback dragCallback = new ViewDragHelper.Callback() {
-        @Override
-        public boolean tryCaptureView(@NonNull View child, int pointerId) {
-            return true; //本View就是要被drag的
-        }
-
-        @Override
-        public int clampViewPositionVertical(View child, int top, int dy) {
-            return top;
-        }
-    };
-    private ViewDragHelper dragger;
-
     private void init(Context context, @Nullable AttributeSet attrs) {
-        ViewGroup parent = (ViewGroup) this.getParent();
-        System.out.println("szw : parent = "+this.getParent()); //=> null
-        dragger = ViewDragHelper.create(parent, dragCallback);
     }
 
     @Override
+    public boolean performClick() {
+        return super.performClick();
+    }
+
+    int downX,downY;
+
+    @Override
     public boolean onTouchEvent(MotionEvent event) {
-        dragger.processTouchEvent(event);
+
+        int action = event.getAction();
+        switch(action){
+            case ACTION_DOWN:
+                downX = (int) event.getX();
+                downY = (int) event.getY();
+                break;
+            case ACTION_MOVE:
+
+                break;
+            case ACTION_UP:
+
+                int upX = (int) event.getX();
+                int upY = (int) event.getY();
+                if( Math.abs(upX - downX) < 10 &&
+                        Math.abs(upY - downY) < 10) {
+                    performClick();
+                }
+
+                break;
+        }
+
         return true;
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
 }
+

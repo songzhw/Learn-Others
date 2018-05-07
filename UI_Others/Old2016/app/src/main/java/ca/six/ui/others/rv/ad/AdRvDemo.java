7da36a51bdf306529d978c4d933f1f294330c117
songzhw <songzhw2012@gmail.com>
2018-05-07 10:36:21 -0400

    rv ad ( basically finish)

--- UI_Others/Old2016/app/src/main/java/ca/six/ui/others/rv/ad/AdRvDemo.java
+++ UI_Others/Old2016/app/src/main/java/ca/six/ui/others/rv/ad/AdRvDemo.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,9 @@ import ca.six.ui.others.rv.OneTypesAdapter;
 import ca.six.ui.others.rv.RvViewHolder;
 
 public class AdRvDemo extends Activity {
+
+    private RecyclerView rv;
+    private LinearLayoutManager linearLayoutManager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,24 +38,53 @@ public class AdRvDemo extends Activity {
         };
 
         List<ItemView> data = new ArrayList<>();
-        for(int i = 0; i < 30; i++){
+        for (int i = 0; i < 30; i++) {
             data.add(new ContentItem(i));
         }
         data.add(12, ad);
 
 
-        RecyclerView rv = findViewById(R.id.rvOne);
-        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv = findViewById(R.id.rvOne);
+        linearLayoutManager = new LinearLayoutManager(this);
+        rv.setLayoutManager(linearLayoutManager);
         OneTypesAdapter adapter = new OneTypesAdapter(data);
         rv.setAdapter(adapter);
+
+        rv.addOnScrollListener(scrollListener);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        rv.removeOnScrollListener(scrollListener);
+    }
+
+    private RecyclerView.OnScrollListener scrollListener = new RecyclerView.OnScrollListener() {
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy);
+
+            int first = linearLayoutManager.findFirstVisibleItemPosition();
+            int last = linearLayoutManager.findLastCompletelyVisibleItemPosition();
+            int rvHeight = linearLayoutManager.getHeight();
+            for (int i = first; i <= last; i++) {
+                View childView = linearLayoutManager.findViewByPosition(i);
+                View ivWeNeed = childView.findViewById(R.id.ivItemAd);
+                if (ivWeNeed != null) {
+                    CoordinateImageView iv = (CoordinateImageView) ivWeNeed;
+                    iv.setDiff(rvHeight - childView.getTop());
+                }
+
+            }
+        }
+    };
 }
 
 class ContentItem implements ItemView {
 
     private final int id;
 
-    public ContentItem(int idx){
+    public ContentItem(int idx) {
         this.id = idx;
     }
 
@@ -63,7 +96,7 @@ class ContentItem implements ItemView {
 
     @Override
     public void bind(RvViewHolder holder) {
-        holder.setText(R.id.tvItemContent, "item "+id);
+        holder.setText(R.id.tvItemContent, "item " + id);
     }
 
 }

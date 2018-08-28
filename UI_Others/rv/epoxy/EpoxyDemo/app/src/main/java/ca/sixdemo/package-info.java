bab1ade26f4ae2c921a39455529a91c6ec90a2d1
songzhw <songzhw2012@gmail.com>
2018-08-27 20:41:44 -0400

    epoxy demo

--- UI_Others/rv/epoxy/EpoxyDemo/app/src/main/java/ca/sixdemo/package-info.java
+++
@@ -1,6 +0,0 @@
-@EpoxyDataBindingLayouts(R.layout.button)
-@PackageModelViewConfig(rClass = R.class)
-package ca.sixdemo;
-
-import com.airbnb.epoxy.EpoxyDataBindingLayouts;
-import com.airbnb.epoxy.PackageModelViewConfig;

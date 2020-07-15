fefc39232e359b6404af6f7d8e6788b409b651ca
songzhw <songzhw2012@gmail.com>
2020-07-15 09:16:46 -0400

    state demo

--- Architecture/LearnArtitecture/app/src/main/java/ca/six/learn/artitecture/flow/statemachine/UIStateMachine.kt
+++ Architecture/LearnArtitecture/app/src/main/java/ca/six/learn/artitecture/flow/statemachine/UIStateMachine.kt
@@ -1,7 +1,9 @@
 package ca.six.learn.artitecture.flow.statemachine
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.lifecycleScope
 import ca.six.learn.artitecture.R
 import kotlinx.coroutines.Dispatchers
@@ -10,24 +12,32 @@ import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 class UIStateMachineDemo : AppCompatActivity(R.layout.activity_btn_tv) {
+    // 使用这个viewModels就得让kotlinOptions的target是java8 (默认是java6)
+    val vm by viewModels<UIStateMachineViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         lifecycleScope.launch {
-
-        }
-
-    }
-
-    // 其实可以是viewmodel调用的方法
-    fun onResult(state: UIState<String>) {
-        when (state) {
-            is Start -> println("start")
-            is Success -> println(state.value)
+            vm.getUser()
+                .collect { event ->  //collect得在被coroutine或suspect函数之中才行!
+                    when (event) {
+                        is Start -> println("szw start : ${Thread.currentThread().name}") //=> 09:15:05, main线程上
+                        is Success -> println("szw succ = ${event.value} : ${Thread.currentThread().name}") //=> 09:15:07, main线程上
+                    }
+                }
         }
     }
 
+}
+
+class UIStateMachineViewModel : ViewModel() {
+    fun getUser() = Machine(Dispatchers.IO) {
+        // http result
+        Thread.sleep(2000)
+        println("szw getUser : ${Thread.currentThread().name}") //=> szw getUser : DefaultDispatcher-worker-1
+        "200"
+    }
 }
 
 

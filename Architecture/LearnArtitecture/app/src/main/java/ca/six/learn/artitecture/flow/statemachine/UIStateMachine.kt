5a985f4801815dea2c9576e9ab62c9d4bc61e419
songzhw <songzhw2012@gmail.com>
2020-07-15 08:08:30 -0400

    add different state, but there is error now

--- Architecture/LearnArtitecture/app/src/main/java/ca/six/learn/artitecture/flow/statemachine/UIStateMachine.kt
+++ Architecture/LearnArtitecture/app/src/main/java/ca/six/learn/artitecture/flow/statemachine/UIStateMachine.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import ca.six.learn.artitecture.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 class UIStateMachineDemo : AppCompatActivity(R.layout.activity_btn_tv) {
 
@@ -17,4 +20,14 @@ class UIStateMachineDemo : AppCompatActivity(R.layout.activity_btn_tv) {
 
     }
 
+}
+
+object BizState {
+    operator fun <T> invoke(actionOn: CoroutineContext = Dispatchers.Default, action: () -> T): Flow<T> {
+        return flow<T>{ emit(action())}
+            .onStart { emit() }
+            .catch { exception -> emit(exception) }
+            .onCompletion { emit() }
+            .flowOn(actionOn)
+    }
 }

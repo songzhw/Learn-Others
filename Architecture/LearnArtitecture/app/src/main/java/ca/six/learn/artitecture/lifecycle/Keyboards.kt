9bf0155e894d0c76aaf1a31a7706a35f9468248c
songzhw <songzhw2012@gmail.com>
2020-07-17 23:14:49 -0400

    keyboard listener

--- Architecture/LearnArtitecture/app/src/main/java/ca/six/learn/artitecture/lifecycle/Keyboards.kt
+++ Architecture/LearnArtitecture/app/src/main/java/ca/six/learn/artitecture/lifecycle/Keyboards.kt
@@ -1,20 +1,29 @@
 package ca.six.learn.artitecture.lifecycle
 
 import android.os.Bundle
+import android.view.View
+import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
+import ca.six.learn.artitecture.R
 
 
-class KeyboardDemo : AppCompatActivity(){
+class KeyboardDemo : AppCompatActivity(R.layout.activity_keyboard) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        lifecycle.addObserver(object: LifecycleEventObserver{
+        lifecycle.addObserver(object : LifecycleEventObserver {
             override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
                 println("szw state change $event")
             }
         })
+
+        val rootView = findViewById<View>(Window.ID_ANDROID_CONTENT)
+        rootView.viewTreeObserver.addOnGlobalLayoutListener {
+            // 每次软软键盘的弹出/消失, 都会触发这个方法
+            println("szw onGlobalLayout")
+        }
     }
 }

c1985959071944eb1a774aec79d2765afd8e4a32
songzhw <songzhw2012@gmail.com>
2020-07-20 22:21:48 -0400

    gesture navigation on Android 10

--- Architecture/LearnArtitecture/app/src/main/java/ca/six/learn/artitecture/lifecycle/Keyboards.kt
+++ Architecture/LearnArtitecture/app/src/main/java/ca/six/learn/artitecture/lifecycle/Keyboards.kt
@@ -38,7 +38,17 @@ fun View.isKeyboardOpen(): Boolean {
     getWindowVisibleDisplayFrame(rect)
     // 以1080 * 2160的手机来说, 正常情况下, rect就是(0, 66 -- 1080, 2028)
     // 要是键盘弹出来了, 那rect就是 (0, 66 -- 1080, 1343)
-    println("szw density = " + Resources.getSystem().displayMetrics.density)
-    println("szw height = ${rootView.height}")
+    // 备注: 2160 - 2028 = 132, 正好是48dp * 2.75(density). 所以navigation bar是48dp
+    println("szw        rect = $rect")
+    println("szw        density = " + Resources.getSystem().displayMetrics.density)
+    println("szw        height = ${rootView.height}")
     return true
 }
+/*
+  1. 可能的例外情况1: gesture navigation  -- 其实不影响我们, 因为我们是判断差大于100dp, 才算出来. 所以不影响正常时
+  仍以1080 * 2160的手机来说,
+  正常情况下, rect就是(0, 66 -- 1080, 2116)      : 这时2160 - 2116 = 44, 只有16dp
+  有键盘弹出时, rect是(0, 66 -- 1080, 1343)
+    (好吧, 这时即使是gesture navigation, 软键盘为了有一个dismiss的图标(向下箭头), 导致nav bar看起来仍成了48dp
+
+ */

3ab39d40144c330d35ce524eeb02090aa278e3a8
songzhw <songzhw2012@gmail.com>
2020-07-20 22:21:59 -0400

    split screen on Andorid N

--- Architecture/LearnArtitecture/app/src/main/java/ca/six/learn/artitecture/lifecycle/Keyboards.kt
+++ Architecture/LearnArtitecture/app/src/main/java/ca/six/learn/artitecture/lifecycle/Keyboards.kt
@@ -51,4 +51,11 @@ fun View.isKeyboardOpen(): Boolean {
   有键盘弹出时, rect是(0, 66 -- 1080, 1343)
     (好吧, 这时即使是gesture navigation, 软键盘为了有一个dismiss的图标(向下箭头), 导致nav bar看起来仍成了48dp
 
+  例外情况2: 分屏
+  1). 当我们应用在下屏时, 正常时是(0, 1061 -- 1080, 2028), 弹出键盘时是(0, 376 - 1080, 1343)
+        (上屏的应用会变得很小, 不再是占一半高度了)
+  2). 当我们应用在上屏时, 键盘弹出只遮住下屏的. 所以我们的应用的onGlobalLayout根本都没有调用到.
+
+  所以整个依赖onGlobalLayout来判断键盘弹出来了的机制是失败的, 因为它不能覆盖到分屏的情况
+  (除非你在manifest中给<application>或<activity>添加android:resizeableActivity=false, 强制不允许分屏)
  */

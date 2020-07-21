99ea5989b8f0f03fa6b4e830e867a3a9d6972be7
songzhw <songzhw2012@gmail.com>
2020-07-20 22:00:46 -0400

    imm.isActive根本不准. 键盘收起来了时也会说是true

--- Architecture/LearnArtitecture/app/src/main/java/ca/six/learn/artitecture/lifecycle/Keyboards.kt
+++ Architecture/LearnArtitecture/app/src/main/java/ca/six/learn/artitecture/lifecycle/Keyboards.kt
@@ -1,8 +1,12 @@
 package ca.six.learn.artitecture.lifecycle
 
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
 import android.view.Window
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
@@ -22,8 +26,19 @@ class KeyboardDemo : AppCompatActivity(R.layout.activity_keyboard) {
 
         val rootView = findViewById<View>(Window.ID_ANDROID_CONTENT)
         rootView.viewTreeObserver.addOnGlobalLayoutListener {
-            // 每次软软键盘的弹出/消失, 都会触发这个方法
-            println("szw onGlobalLayout")
+            // 每次软软键盘的弹出/消失, 都会触发onGlobalLayout()这个方法
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            println("szw onGlobalLayout(${imm.isActive}) : ${rootView.isKeyboardOpen()}")
         }
     }
+}
+
+fun View.isKeyboardOpen(): Boolean {
+    val rect = Rect()
+    getWindowVisibleDisplayFrame(rect)
+    // 以1080 * 2160的手机来说, 正常情况下, rect就是(0, 66 -- 1080, 2028)
+    // 要是键盘弹出来了, 那rect就是 (0, 66 -- 1080, 1343)
+    println("szw density = " + Resources.getSystem().displayMetrics.density)
+    println("szw height = ${rootView.height}")
+    return true
 }
